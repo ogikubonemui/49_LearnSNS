6e77870f624a2491c864458dd2469a73d0f053b4
yasuyukitakeshita <thetake1206@gmail.com>
2019-01-15 11:12:29 +0800

    パスワードと画像のバリデーション追加

--- register/signup.php
+++ register/signup.php
@@ -22,11 +22,24 @@ if (!empty($_POST)){
     if($email == ''){
         $errors['email'] = 'blank';
     }
+    //パスワードの文字数を数える
+    //hogehogeと入力した場合には$countには8が入る
+    $count = strlen($password);
     if($password == ''){
         $errors['password'] = 'blank';
+    }elseif($count < 4 || 16 < $count){
+        // ||パイプ演算子を使って4文字未満または16文字より多い場合エラー
+        $errors['password'] = 'length';
+    }
+    //$_FILES[キー]['name']; ファイル名
+    //$_FILES[キー]['tmp_name']; ファイルデータそのもの
+    $file_name = $_FILES['input_img_name']['name'];
+    if(!empty($file_name)){
+    //ファイルの処理
+    }else{
+        $errors['img_name'] = 'blank';
     }
 }
-
 
 ?>
 <!DOCTYPE html>
@@ -46,34 +59,41 @@ if (!empty($_POST)){
                 <!--
                     まずformタグのmethodとactionを確認
                     sign.upでバリデーションをする
+                    ファイルをアップロードする際の必須ルール
+                    1.POST送信であること
+                    2.enctype（エンコードタイプ）属性にmultipart/form-dataが設定されていること
                 -->
                 <form method="POST" action="signup.php" enctype="multipart/form-data">
                     <div class="form-group">
                         <label for="name">ユーザー名</label>
-                        <input type="text" name="input_name" class="form-control" id="name" placeholder="山田 太郎"
-                            value="">
-                            <?php if(isset($errors['name']) && $errors['name'] == 'blank'): ?>
-                                <p class="text-danger">ユーザー名を入力してください</p>
-                            <?php endif; ?>
+                        <input type="text" name="input_name" class="form-control" id="name" placeholder="山田 太郎" value="">
+                        <?php if(isset($errors['name']) && $errors['name'] == 'blank'): ?>
+                            <p class="text-danger">ユーザー名を入力してください</p>
+                        <?php endif; ?>
                     </div>
                     <div class="form-group">
                         <label for="email">メールアドレス</label>
-                        <input type="email" name="input_email" class="form-control" id="email" placeholder="example@gmail.com"
-                            value="">
-                            <?php if(isset($errors['email']) && $errors['email'] == 'blank'): ?>
-                                <p class="text-danger">メールアドレスを入力してください</p>
-                            <?php endif; ?>
+                        <input type="email" name="input_email" class="form-control" id="email" placeholder="example@gmail.com" value="">
+                        <?php if(isset($errors['email']) && $errors['email'] == 'blank'): ?>
+                            <p class="text-danger">メールアドレスを入力してください</p>
+                        <?php endif; ?>
                     </div>
                     <div class="form-group">
                         <label for="password">パスワード</label>
                         <input type="password" name="input_password" class="form-control" id="password" placeholder="4 ~ 16文字のパスワード">
-                            <?php if(isset($errors['password']) && $errors['password'] == 'blank'): ?>
-                                <p class="text-danger">パスワードを入力してください</p>
-                            <?php endif; ?>
+                        <?php if(isset($errors['password']) && $errors['password'] == 'blank'): ?>
+                            <p class="text-danger">パスワードを入力してください</p>
+                        <?php endif; ?>
+                        <?php if(isset($errors['password']) && $errors['password'] == 'length'): ?>
+                            <p class="text-danger">パスワードは4 ~ 16文字で入力してください</p>
+                        <?php endif; ?>
                     </div>
                     <div class="form-group">
                         <label for="img_name">プロフィール画像</label>
                         <input type="file" name="input_img_name" id="img_name" accept="image/*">
+                        <?php if(isset($errors['img_name']) && $errors['img_name'] == 'blank'): ?>
+                            <p class="text-danger">画像を選択してください</p>
+                        <?php endif; ?>
                     </div>
                     <input type="submit" class="btn btn-default" value="確認">
                     <span style="float: right; padding-top: 6px;">ログインは
